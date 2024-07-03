990731eb0a1b1c1c82ca808c3e8be2e0934de560
Michael Hart <michael.hart1994@gmail.com>
2024-07-03 00:08:47 -0700

    First attempt to fix copiously

--- src/main/java/net/gargoyle/copiousfix/mixin/CopiousStackMixin.java
+++ src/main/java/net/gargoyle/copiousfix/mixin/CopiousStackMixin.java
@@ -1,12 +1,30 @@
 package net.gargoyle.copiousfix.mixin;
 
-import iskallia.vault.gear.attribute.custom.EffectCloudAttribute;
+import iskallia.vault.block.VaultOreBlock;
 
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(EffectCloudAttribute.EffectCloud.class)
-public interface EffectCloudAccessor {
-    @Accessor
-    String getTooltip();
+import java.util.ArrayList;
+import java.util.List;
+
+@Mixin(VaultOreBlock.class)
+public class CopiousStackMixin {
+    @Inject(method = "getDrops", at = @At("RETURN"))
+    public void onGetDrops(CallbackInfoReturnable<List<ItemStack>> cir) {
+        List<ItemStack> returned = cir.getReturnValue();
+        if (returned.size() < 2) {
+            // No copiously proc; leave it as it is
+            return;
+        }
+
+        List<ItemStack> replacementStack = new ArrayList<>();
+        for (ItemStack stack : returned) {
+            replacementStack.add(new ItemStack(stack.getItem(), stack.getCount()));
+        }
+        cir.setReturnValue(replacementStack);
+    }
 }
