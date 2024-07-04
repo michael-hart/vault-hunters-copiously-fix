e6634c2552e8bd6e6f14cb43a9b6d699fe39cd1c
Michael Hart <michael.hart1994@gmail.com>
2024-07-03 21:27:39 -0700

    Minor corrections to build files

--- src/main/java/net/gargoyle/copiousfix/mixin/CopiousStackMixin.java
+++ src/main/java/net/gargoyle/copiousfix/mixin/CopiousStackMixin.java
@@ -9,6 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +27,7 @@ public class CopiousStackMixin {
         List<ItemStack> replacementStack = new ArrayList<>();
 //        final Logger LOGGER = LogUtils.getLogger();
         for (ItemStack stack : returned) {
+//            Can we print the name of the player?
 //            LOGGER.info("Duplicating item stack: {}", stack);
             replacementStack.add(new ItemStack(stack.getItem(), stack.getCount()));
         }
